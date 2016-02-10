d45c3ae003ca092dcc8c28a77e114ff2e757e5e8
juanpablo.bosnjak <juanpablo.bosnjak@intraway.com>
2016-02-10 16:33:17 -0300

    as

--- golang/src/testApi.go
+++ golang/src/testApi.go
@@ -1,38 +1,17 @@
-
 package main
 
 import (
     "database/sql"
     _ "github.com/go-sql-driver/mysql"
 	"net/http"
-	//"log"
 	"fmt"
 )
 
 func main() {
-	
 	http.HandleFunc("/testApiPing", apiPing)
 	http.HandleFunc("/testApiDB", apiDB)
     http.ListenAndServe(":8082", nil)
-
-   
 }
-
-func apiPing(w http.ResponseWriter, r * http.Request){
-	
-	param1 := r.URL.Query().Get("param1")
-
-    w.Write([]byte(Reverse(param1)));
-}
-
-func Reverse(s string) string {
-	r := []rune(s)
-	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
-		r[i], r[j] = r[j], r[i]
-	}
-	return string(r)
-}
-
 
 func apiDB(w http.ResponseWriter, r * http.Request){
 	
@@ -63,4 +42,19 @@ func apiDB(w http.ResponseWriter, r * http.Request){
 
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintln(w, "Response: ", col1)
+}
+
+func apiPing(w http.ResponseWriter, r * http.Request){
+	
+	param1 := r.URL.Query().Get("param1")
+
+    w.Write([]byte(Reverse(param1)));
+}
+
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
 }
