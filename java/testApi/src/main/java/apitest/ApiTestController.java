9b4ad4b527b4872b90ed43259c99aa62f6c6bd55
juanpablo.bosnjak <juanpablo.bosnjak@intraway.com>
2016-02-10 12:49:54 -0300

    refactor

--- java/testApi/src/main/java/apitest/ApiTestController.java
+++ java/testApi/src/main/java/apitest/ApiTestController.java
@@ -8,28 +8,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 @RestController
 public class ApiTestController {
 
-    @RequestMapping("/testApiDB")
-    public String testApiDB(@RequestParam(value="user", defaultValue="1") long id) {
-        
+	@RequestMapping("/testApiPing")
+	public String testApiPing(
+			@RequestParam(value = "param1", defaultValue = "1") String id) {
+
+		return new StringBuilder(id).reverse().toString();
+	}
+
+	@Autowired
+	private UserDao userDao;
+
+	@RequestMapping("/testApiDB")
+	public String testApiDB(
+			@RequestParam(value = "user", defaultValue = "1") long id) {
+
 		try {
 			User user = userDao.findOne(id);
-			if (user == null){
+			if (user == null) {
 				return "User Not Found";
 			}
 			return user.getContent().toString();
-		}
-		catch (Exception ex) {
+		} catch (Exception ex) {
 			ex.printStackTrace();
 			return "User Not Found";
 		}
-    }
-    
-    @RequestMapping("/testApiPing")
-    public String testApiPing(@RequestParam(value="param1", defaultValue="1") String id) {
-        
-		return new StringBuilder(id).reverse().toString();
-    }
-	
-	 @Autowired
-	private UserDao userDao;
+	}
 }
