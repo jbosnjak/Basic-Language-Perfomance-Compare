8e815f7a596bad90d9451f4b61bb1de206cda16e
juanpablo.bosnjak <juanpablo.bosnjak@intraway.com>
2016-02-12 15:08:53 -0300

    fix

--- php/testApiDB.php
+++ php/testApiDB.php
@@ -2,7 +2,7 @@
 $userId = $_GET ["user"];
 
 $mysqli = new mysqli ( "127.0.0.1", "root", "", "test" );
-$res = $mysqli->query ( "SELECT maketxt FROM testtable WHERE idtesttable = " . $userId );
+$res = $mysqli->query ( "SELECT content FROM testtable WHERE idtesttable = " . $userId );
 $row = $res->fetch_assoc ();
 echo json_encode($row);
 $mysqli->close();
