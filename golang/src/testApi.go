732b7f80aa38c2eec1d0be2b331634e2be64c3d1
juanpablo.bosnjak <juanpablo.bosnjak@intraway.com>
2016-02-12 09:40:29 -0300

    go fix

--- golang/src/testApi.go
+++ golang/src/testApi.go
@@ -5,18 +5,58 @@ import (
     _ "github.com/go-sql-driver/mysql"
 	"github.com/gin-gonic/gin"
 	"net/http"
-	//"fmt"
 )
 
 func main() {
-	//http.HandleFunc("/testApiPing", apiPing)
-	//http.HandleFunc("/testApiDB", apiDB)
-    //http.ListenAndServe(":8082", nil)
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.New()
 	r.GET("/testApiPing", apiPing)
 	r.GET("/testApiDB", apiDB)
     r.Run(":8082")
+}
+
+func apiDB(c *gin.Context){
+	userId := c.Query("user")
+	
+ 	db, err := sql.Open("mysql", "root@/test")
+    if err != nil {
+        panic(err.Error())
+    }
+    defer db.Close()
+
+	stmt, err := db.Prepare("SELECT content FROM testtable WHERE idtesttable = ?")
+
+    res, err := stmt.Query(userId)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var col1 string
+	res.Next();
+	err = res.Scan(&col1);
+
+	if err != nil {
+		panic(err.Error())
+	}
+	defer res.Close()
+
+	c.String(http.StatusOK, "Response: %s", col1)
+}
+
+
+func apiPing(c *gin.Context){
+	param1 := c.Query("param1")
+	responseMessage := Reverse(param1)
+    c.String(http.StatusOK, responseMessage )
+}
+
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
 }
 
 /*func apiDB(w http.ResponseWriter, r * http.Request){
@@ -50,54 +90,3 @@ func main() {
 	fmt.Fprintln(w, "Response: ", col1)
 	r.Header.Set("Connection", "close")
 }*/
-
-func apiDB(c *gin.Context){
-	userId := c.Param("user")
-	
- 	db, err := sql.Open("mysql", "root@/test")
-    if err != nil {
-        panic(err.Error())
-    }
-    defer db.Close()
-
-	stmt, err := db.Prepare("SELECT content FROM testtable WHERE idtesttable = ?")
-
-    res, err := stmt.Query(userId)
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	var col1 string
-	res.Next();
-	err = res.Scan(&col1);
-
-	if err != nil {
-		panic(err.Error())
-	}
-	defer res.Close()
-
-	c.String(http.StatusOK, "Response: %s", col1)
-}
-
-/*func apiPing(w http.ResponseWriter, r * http.Request){
-	
-	param1 := r.URL.Query().Get("param1")
-
-    w.Write([]byte(Reverse(param1)));
-	
-	r.Header.Set("Connection", "close")
-}*/
-
-func apiPing(c *gin.Context){
-	param1 := c.Param("param1")
-    c.String(http.StatusOK, Reverse(param1))
-}
-
-func Reverse(s string) string {
-	r := []rune(s)
-	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
-		r[i], r[j] = r[j], r[i]
-	}
-	return string(r)
-}
